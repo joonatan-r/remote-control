b61aee0b09cdea72cd697ff97e55928826dabf14
joonatan-r <joonatan.rantanen@gmail.com>
2023-12-08 00:30:43 +0200

    basic ui

--- client/main.js
+++ client/main.js
@@ -1,16 +1,25 @@
 
 let imgLoaded = false;
+let naturalHeight = 0;
+let naturalWidth = 0;
 const img = document.createElement('img');
 img.src = window.location.origin + '/img';
-img.onload = () => { imgLoaded = true; };
-img.onclick = (e) => {
+img.onload = () => {
+    imgLoaded = true;
+    if (naturalHeight === 0 || naturalWidth === 0) {
+        // store only once as seem to be able to be 0 if not fully loaded 
+        naturalHeight = img.naturalHeight;
+        naturalWidth = img.naturalWidth;
+    }
+};
+img.onmousedown = (e) => {
     const rect = e.target.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
     const relativeX = x / rect.width;
     const relativeY = y / rect.height;
-    const pixelX = relativeX * img.naturalWidth;
-    const pixelY = relativeY * img.naturalHeight;
+    const pixelX = relativeX * naturalWidth;
+    const pixelY = relativeY * naturalHeight;
     fetch(window.location.origin + '/mouseMove?arg1=' + pixelX + '&arg2=' + pixelY);
 }
 document.body.appendChild(img);
@@ -21,7 +30,7 @@ mouse.style.top = -50;
 mouse.style.left = -50;
 mouse.style.margin = '0px';
 mouse.style.padding = '0px';
-mouse.innerHTML = '^';
+mouse.innerHTML = 'V';
 document.body.appendChild(mouse);
 
 async function updateMouse() {
@@ -43,3 +52,25 @@ setInterval(() => {
         updateMouse();
     }
 }, 500);
+
+const pressed = {};
+const selection = document.getElementById('selection');
+const pressButton = document.getElementById('pressButton');
+pressButton.onclick = () => {
+    const currentVal = selection.options[selection.selectedIndex].value;
+    const action = pressed[currentVal] ? 'u' : 'd';
+
+    if (currentVal === 'mouseLeft') {
+        fetch(window.location.origin + '/mousePress?arg1=l&arg2=' + action);
+    } else if (currentVal === 'mouseRight') {
+        fetch(window.location.origin + '/mousePress?arg1=r&arg2=' + action);
+    } else {
+        fetch(window.location.origin + '/press?arg1=' + currentVal + '&arg2=' + action);
+    }
+    pressed[currentVal] = !pressed[currentVal];
+    pressButton.innerHTML = pressed[currentVal] ? 'Up' : 'Down';
+};
+selection.onchange = () => {
+    const currentVal = selection.options[selection.selectedIndex].value;
+    pressButton.innerHTML = pressed[currentVal] ? 'Up' : 'Down';
+};
